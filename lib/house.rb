44ac5bebfa7a5f37075ed4583f720591edb1c54a
SamsonDeVol <devols@oregonstate.edu>
2022-04-26 13:27:02 -0700

    consolidate line numbers 1 and 2 in method line change else/if format to switch statement in method phrase

--- lib/house.rb
+++ lib/house.rb
@@ -1,36 +1,33 @@
 class House
 
   def line(number)
-    if number == 1
-      "This is the house that Jack built.\n"
-    else
-      "This is #{phrase(number)}the house that Jack built.\n"
-    end
+    "This is #{phrase(number)}the house that Jack built.\n"
   end
 
   def phrase(number)
     result = ""
-    if number == 12
+    case number
+    when 12
       result = "the horse and the hound and the horn that belonged to " + phrase(number-1)
-    elsif number == 11
+    when 11
       result = "the farmer sowing his corn that kept " + phrase(number-1)
-    elsif number == 10
+    when 10
       result = "the rooster that crowed in the morn that woke " + phrase(number-1)
-    elsif number == 9
+    when 9
       result = "the priest all shaven and shorn that married " + phrase(number-1)
-    elsif number == 8
+    when 8
       result = "the man all tattered and torn that kissed " + phrase(number-1)
-    elsif number == 7
+    when 7
       result = "the maiden all forlorn that milked " + phrase(number-1)
-    elsif number == 6
+    when 6
       result = "the cow with the crumpled horn that tossed " + phrase(number-1)
-    elsif number == 5
+    when 5
       result = "the dog that worried " + phrase(number-1)
-    elsif number == 4
+    when 4
       result = "the cat that killed " + phrase(number-1)
-    elsif number == 3
+    when 3
       result = "the rat that ate " + phrase(number-1)
-    elsif number == 2
+    when 2
       result = "the malt that lay in " + phrase(number-1)
     end
     return result
