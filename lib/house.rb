fb63af42a5e0d61fe4e3f74035ed6d1f452fca5a
SamsonDeVol <devols@oregonstate.edu>
2022-05-17 19:10:54 -0700

    Introduce an array structure to contain phrases

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,14 @@
 class House
 
+  PHRASES = [
+    "the malt that lay in ", "the rat that ate ", 
+    "the cat that killed ", "the dog that worried ", 
+    "the cow with the crumpled horn that tossed ", 
+    "the maiden all forlorn that milked ", "the man all tattered and torn that kissed ", 
+    "the priest all shaven and shorn that married ", "the rooster that crowed in the morn that woke ", 
+    "the farmer sowing his corn that kept ", "the horse and the hound and the horn that belonged to "
+  ] 
+
   def line(number)
     "This is #{phrase(number)}the house that Jack built.\n"
   end
@@ -8,27 +17,27 @@ class House
     result = ""
     case number
     when 12
-      result = "the horse and the hound and the horn that belonged to " + phrase(number-1)
+      result = PHRASES[10] + phrase(number-1)
     when 11
-      result = "the farmer sowing his corn that kept " + phrase(number-1)
+      result = PHRASES[9] + phrase(number-1)
     when 10
-      result = "the rooster that crowed in the morn that woke " + phrase(number-1)
+      result = PHRASES[8] + phrase(number-1)
     when 9
-      result = "the priest all shaven and shorn that married " + phrase(number-1)
+      result = PHRASES[7] + phrase(number-1)
     when 8
-      result = "the man all tattered and torn that kissed " + phrase(number-1)
+      result = PHRASES[6] + phrase(number-1)
     when 7
-      result = "the maiden all forlorn that milked " + phrase(number-1)
+      result = PHRASES[5] + phrase(number-1)
     when 6
-      result = "the cow with the crumpled horn that tossed " + phrase(number-1)
+      result = PHRASES[4] + phrase(number-1)
     when 5
-      result = "the dog that worried " + phrase(number-1)
+      result = PHRASES[3] + phrase(number-1)
     when 4
-      result = "the cat that killed " + phrase(number-1)
+      result = PHRASES[2] + phrase(number-1)
     when 3
-      result = "the rat that ate " + phrase(number-1)
+      result = PHRASES[1] + phrase(number-1)
     when 2
-      result = "the malt that lay in " + phrase(number-1)
+      result = PHRASES[0]
     end
     return result
   end
