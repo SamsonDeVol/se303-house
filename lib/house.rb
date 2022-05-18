aee706e8f48d336172142a72cc77f2c06142c894
SamsonDeVol <devols@oregonstate.edu>
2022-05-17 19:42:15 -0700

    Create a Lyrics class, copy relevant methods and data from House class

--- lib/house.rb
+++ lib/house.rb
@@ -25,3 +25,30 @@ class House
     1.upto(12).map { |number| line(number) }.join("\n")
   end
 end
+
+class Lyrics
+
+  PHRASES = [
+    "the malt that lay in ", "the rat that ate ", 
+    "the cat that killed ", "the dog that worried ", 
+    "the cow with the crumpled horn that tossed ", 
+    "the maiden all forlorn that milked ", "the man all tattered and torn that kissed ", 
+    "the priest all shaven and shorn that married ", "the rooster that crowed in the morn that woke ", 
+    "the farmer sowing his corn that kept ", "the horse and the hound and the horn that belonged to "
+  ] 
+
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def phrase(number)
+    result = ""
+    for i in number.downto(2) 
+      result += PHRASES[i-2]
+    end
+    result
+  end 
+
+end
